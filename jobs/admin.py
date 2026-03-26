61bad47a8cd281b550e82724109925d21d8c2d13
vikidmkn <victoria.dimkina@gmail.com>
2026-03-26 19:57:06 +0200

    jobs/admin.py added

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Company, Job, UserJobInteraction
 
-# Register your models here.
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_verified_inclusive', 'website')
+    search_fields = ('name',)
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('title', 'company', 'job_type', 'location', 'is_translated')
+    list_filter = ('job_type', 'is_translated', 'company')
+    search_fields = ('title', 'company__name')
+
+@admin.register(UserJobInteraction)
+class UserJobInteractionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'job', 'status', 'created_at')
