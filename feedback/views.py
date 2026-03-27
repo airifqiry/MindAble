13db35502dfbaab2ff01a505f44c68e28772a9f3
vikidmkn <victoria.dimkina@gmail.com>
2026-03-27 18:28:09 +0200

    changes in almost everything

--- feedback/views.py
+++ feedback/views.py
@@ -7,11 +7,9 @@ class JobFeedbackListCreate(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        # users only ever see their own feedback
         return JobFeedback.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
-        # automatically attach the logged-in user
         serializer.save(user=self.request.user)
 
 
